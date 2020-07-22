655aa9502d914e7cc469061b992634da4b2709d4
Jp1200 <jprkr96@gmail.com>
2020-07-22 14:15:51 -0500

    class reimagined

--- neurons.py
+++ neurons.py
@@ -1,39 +1,32 @@
 import numpy as np
 
+np.random.seed(0)
 
-class Neuron:
-    def __init__(self, inputs, weights):
-        self.inputs = inputs
-        self.weights = weights
-
-    def single_output(self, a, b, bias):
-        output = np.dot(a, b) + bias
-        print(output)
-        return output
-
-# inputs = [1, 2, 3, 2.5]
-# weights = [[0.3, 0.1, -0.5, 1.0],
-#            [0.4, -0.91, 0.25, -0.5],
-#            [-0.26, -0.21, 0.17, 0.87]]
-# biases = [2, 3, 0.5]
+X = [[1, 2, 3, 2.5],
+     [2.0, 5.0, -1.0, 2.0],
+     [-1.5, 2.7, 3.3, -0.8]]
+# generally want small values randomly assigned betwen -1 and 1
+# Why? input data is 1.2 or 5 or larger value data will exponentially increase weights and values
+# want to scale and normalize so the scale remains managable and the meaning is still conveyed
 
 
-first = Neuron([1, 2, 3, 2.5], [[0.3, 0.1, -0.5, 1.0],
-                                [0.4, -0.91, 0.25, -0.5],
-                                [-0.26, -0.21, 0.17, 0.87]])
-first.single_output(first.weights, first.inputs, [1.2, 3, -0.4])
-# print(output)
+class Layer:
+    def __init__(self, n_inputs, n_neurons):
+
+        self.weights = 0.10*np.random.randn(n_inputs, n_neurons)
+        self.biases = np.zeros((1, n_neurons))
+
+    def forward(self, inputs):
+        self.output = np.dot(inputs, self.weights) + self.biases
 
 
-# layer_outputs = []
-# for neuron_weights, neuron_bias in zip(weights, biases):
-#     neuron_output = 0
-#     for n_input, weight in zip(inputs, neuron_weights):
-#         neuron_output += n_input*weight
-#     neuron_output += neuron_bias
-#     layer_outputs.append(neuron_output)
-# print(layer_outputs)
+layer1 = Layer(4, 5)
+layer2 = Layer(5, 2)
 
+layer1.forward(X)
+# print(layer1.output)
+layer2.forward(layer1.output)
+print(layer2.output)
 
 # Inputs could be layers
 #
